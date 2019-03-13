624143be54b1ecbb27be156703e8c1a8ab6c261b
StephenCzarnecki <sczarnecki@lbl.gov>
2019-03-13 13:29:45 -0400

    Updating unit test

--- test/calc_from_disk.unit.cpp
+++ test/calc_from_disk.unit.cpp
@@ -80,7 +80,8 @@ TEST_F(TestCalcFromDisk, Test_NFRC_103_103)
     EXPECT_NEAR(u_result.layer_solar_absorptances[1], 0.072235301276579358, 1e-14);
 
     Thermal_Result shgc_result = calc_shgc(product_paths, gaps, standard_path, 1.0, 1.0);
-    EXPECT_NEAR(shgc_result.result, 0.86058891721415542, 1e-14);
-    EXPECT_NEAR(shgc_result.t_sol, 0.83380702773635118, 1e-14);
-    EXPECT_NEAR(shgc_result.layer_solar_absorptances[0], 0.091376375800192963, 1e-14);
+    EXPECT_NEAR(shgc_result.result, 0.76323563155897300, 1e-14);
+    EXPECT_NEAR(shgc_result.t_sol, 0.70324342292094888, 1e-14);
+    EXPECT_NEAR(shgc_result.layer_solar_absorptances[0], 0.096478606342105686, 1e-14);
+    EXPECT_NEAR(shgc_result.layer_solar_absorptances[1], 0.072235301276579358, 1e-14);
 }
