d20aa503d4d64af7fd7bd038a069d12d8515d333
StephenCzarnecki <sczarnecki@lbl.gov>
2021-03-30 19:27:03 -0400

    Added constructors for environmental conditions.

--- src/environmental_conditions.cpp
+++ src/environmental_conditions.cpp
@@ -2,6 +2,29 @@
 
 namespace wincalc
 {
+    Environment::Environment(double air_temperature,
+                             double pressure,
+                             double convection_coefficient,
+                             Tarcog::ISO15099::BoundaryConditionsCoeffModel coefficient_model,
+                             double radiation_temperature,
+                             double emissivity,
+                             double air_speed,
+                             Tarcog::ISO15099::AirHorizontalDirection air_direction,
+                             double direct_solar_radiation) :
+        air_temperature(air_temperature),
+        pressure(pressure),
+        convection_coefficient(convection_coefficient),
+        coefficient_model(coefficient_model),
+		radiation_temperature(radiation_temperature),
+		emissivity(emissivity),
+		air_speed(air_speed),
+		air_direction(air_direction),
+		direct_solar_radiation(direct_solar_radiation)
+    {}
+
+	Environments::Environments(Environment outside, Environment inside) : outside(outside), inside(inside)
+	{}
+
     Environments nfrc_u_environments()
     {
         Environment inside{294.15,
--- src/environmental_conditions.h
+++ src/environmental_conditions.h
@@ -6,6 +6,16 @@ namespace wincalc
 {
     struct Environment
     {
+        Environment(double air_temperature,
+                    double pressure,
+                    double convection_coefficient,
+                    Tarcog::ISO15099::BoundaryConditionsCoeffModel coefficient_model,
+                    double radiation_temperature,
+                    double emissivity,
+                    double air_speed = 0.0,
+                    Tarcog::ISO15099::AirHorizontalDirection air_direction =
+                      Tarcog::ISO15099::AirHorizontalDirection::None,
+                    double direct_solar_radiation = 0.0);
         double air_temperature;
         double pressure;
         double convection_coefficient;
@@ -21,11 +31,12 @@ namespace wincalc
 
     struct Environments
     {
+		Environments(Environment outside, Environment inside);
         Environment outside;
         Environment inside;
     };
 
-	Environments nfrc_u_environments();
+    Environments nfrc_u_environments();
     Environments nfrc_shgc_environments();
 
 }   // namespace wincalc
