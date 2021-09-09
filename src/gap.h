c5b2aa8ac92c8e6e35c8bb81a836091a6f2cfd1d
vidanovic <dvvidanovic@lbl.gov>
2021-09-09 10:12:17 -0700

    Gap pressure is now passed to the gap as well as creation of support pillars

--- src/gap.h
+++ src/gap.h
@@ -1,8 +1,10 @@
 #ifndef WINCALC_GAP_H
 #define WINCALC_GAP_H
 
-#include <WCEGases.hpp>
 #include <variant>
+#include <WCEGases.hpp>
+#include <WCETarcog.hpp>
+
 
 namespace wincalc
 {
@@ -18,25 +20,51 @@ namespace wincalc
         double percent;
     };
 
+    struct Pillar
+    {
+        virtual ~Pillar() = default;
+        virtual std::shared_ptr<Tarcog::ISO15099::CIGUGapLayer>
+          createGapPillar(const std::shared_ptr<Tarcog::ISO15099::CIGUGapLayer> & gap) = 0;
+        double conductivity;        
+    };
+
+    struct Pillar_Circular : Pillar
+    {
+        std::shared_ptr<Tarcog::ISO15099::CIGUGapLayer>
+          createGapPillar(const std::shared_ptr<Tarcog::ISO15099::CIGUGapLayer> & gap) override;
+        double spacing;
+        double radius;
+    };
+
     struct Engine_Gap_Info
     {
-        Engine_Gap_Info(Gases::CGasData const & gas, double thickness, double pressure = Gases::DefaultPressure);
+        Engine_Gap_Info(Gases::CGasData const & gas,
+                        double thickness,
+                        double pressure = Gases::DefaultPressure,
+                        std::shared_ptr<Pillar> pillar = nullptr);
         Engine_Gap_Info(std::vector<Engine_Gas_Mixture_Component> const & gases,
                         double thickness,
-                        double pressure = Gases::DefaultPressure);
-        Engine_Gap_Info(Gases::GasDef const & gas, double thickness, double pressure = Gases::DefaultPressure);
+                        double pressure = Gases::DefaultPressure,
+                        std::shared_ptr<Pillar> pillar = nullptr);
+        Engine_Gap_Info(Gases::GasDef const & gas,
+                        double thickness,
+                        double pressure = Gases::DefaultPressure,
+                        std::shared_ptr<Pillar> pillar = nullptr);
         Engine_Gap_Info(std::vector<Predefined_Gas_Mixture_Component> const & gases,
                         double thickness,
-                        double pressure = Gases::DefaultPressure);
+                        double pressure = Gases::DefaultPressure,
+                        std::shared_ptr<Pillar> pillar = nullptr);
         Engine_Gap_Info(std::vector<std::variant<Predefined_Gas_Mixture_Component,
                                                  Engine_Gas_Mixture_Component>> const & gases,
                         double thickness,
-                        double pressure = Gases::DefaultPressure);
+                        double pressure = Gases::DefaultPressure,
+                        std::shared_ptr<Pillar> pillar = nullptr);
 
 
         std::vector<Engine_Gas_Mixture_Component> gases;
         double thickness;
         double pressure;
+        std::shared_ptr<Pillar> pillar;
     };
 }   // namespace wincalc
 #endif
