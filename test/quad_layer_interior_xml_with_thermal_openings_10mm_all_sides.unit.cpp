88d73b313c9473ebad95d4ff8df3e406bd542fda
StephenCzarnecki <sczarnecki@lbl.gov>
2022-07-22 12:36:10 -0400

    Adding test for 4-layer system with xml shade with thermal openings on sides based on system from Bruno from Fraunhofer.

--- test/quad_layer_interior_xml_with_thermal_openings_10mm_all_sides.unit.cpp
+++ test/quad_layer_interior_xml_with_thermal_openings_10mm_all_sides.unit.cpp
@@ -16,7 +16,7 @@
 using namespace wincalc;
 using namespace window_standards;
 
-class TestBSDFThermalOpenness : public testing::Test
+class TestQuadLayerInteriorXMLWithThermalOpenings : public testing::Test
 {
 protected:
     std::shared_ptr<Glazing_System> glazing_system_u;
@@ -28,9 +28,17 @@ protected:
         clear_3_path /= "products";
         clear_3_path /= "CLEAR_3.json";
 
+		std::filesystem::path SolarEPlusArBl6_path(test_dir);
+		SolarEPlusArBl6_path /= "products";
+		SolarEPlusArBl6_path /= "SolarEPlusArBl6.json";
+
+		std::filesystem::path CG_Prem2_5_path(test_dir);
+		CG_Prem2_5_path /= "products";
+		CG_Prem2_5_path /= "CG_Prem2_5.json";
+
         std::filesystem::path shade_path(test_dir);
-		shade_path /= "products";
-		shade_path /= "2011-SA1.XML";
+        shade_path /= "products";
+        shade_path /= "shade_full.xml";
 
         std::vector<std::variant<std::shared_ptr<OpticsParser::ProductData>,
                                  wincalc::Product_Data_Optical_Thermal>>
@@ -39,15 +47,29 @@ protected:
         auto clear_3 = OpticsParser::parseJSONFile(clear_3_path.string());
         auto shade = OpticsParser::parseBSDFXMLFile(shade_path.string());
         auto converted_shade = wincalc::convert_to_solid_layer(shade);
-		converted_shade.thermal_data->opening_top = 0.01;
-		converted_shade.thermal_data->opening_bottom = 0.01;
-		converted_shade.thermal_data->opening_left = 0;
-		converted_shade.thermal_data->opening_right = 0;
+        converted_shade.thermal_data->opening_top = 0.01;
+        converted_shade.thermal_data->opening_bottom = 0.01;
+        converted_shade.thermal_data->opening_left = 0.01;
+        converted_shade.thermal_data->opening_right = 0.01;
+
+		auto SolarEPlusArBl6 = OpticsParser::parseJSONFile(SolarEPlusArBl6_path.string());
+		auto CG_Prem2_5 = OpticsParser::parseJSONFile(CG_Prem2_5_path.string());
+
+		products.push_back(SolarEPlusArBl6);
         products.push_back(clear_3);
-		products.push_back(converted_shade);
+		products.push_back(CG_Prem2_5);
+        products.push_back(converted_shade);
 
         Engine_Gap_Info air_gap(Gases::GasDef::Air, 0.0127);
+        Predefined_Gas_Mixture_Component air_10_percent{Gases::GasDef::Air, .1};
+        Predefined_Gas_Mixture_Component argon_90_percent{Gases::GasDef::Argon, .9};
+		std::vector<Predefined_Gas_Mixture_Component> gas_mix_components;
+		gas_mix_components.push_back(air_10_percent);
+		gas_mix_components.push_back(argon_90_percent);
+        Engine_Gap_Info argon_mix(gas_mix_components, .0127);
         std::vector<Engine_Gap_Info> gaps;
+        gaps.push_back(argon_mix);
+        gaps.push_back(argon_mix);
         gaps.push_back(air_gap);
 
         std::filesystem::path standard_path(test_dir);
@@ -65,15 +87,15 @@ protected:
     }
 };
 
-TEST_F(TestBSDFThermalOpenness, Test_Thermal)
+TEST_F(TestQuadLayerInteriorXMLWithThermalOpenings, Test_Thermal)
 {
     test_thermal_results(
-      "thermal_openings_xml_shade_10mm_top_and_bottom", "thermal_U_Environment", glazing_system_u, update_results);
+      "quad_layer_interior_xml_with_thermal_openings_10mm_all_sides", "thermal_U_Environment", glazing_system_u, update_results);
     test_thermal_results(
-      "thermal_openings_xml_shade_10mm_top_and_bottom", "thermal_SHGC_Environment", glazing_system_shgc, update_results);
+      "quad_layer_interior_xml_with_thermal_openings_10mm_all_sides", "thermal_SHGC_Environment", glazing_system_shgc, update_results);
 }
 
-TEST_F(TestBSDFThermalOpenness, Test_Optical)
+TEST_F(TestQuadLayerInteriorXMLWithThermalOpenings, Test_Optical)
 {
-    test_optical_results("thermal_openings_xml_shade_10mm_top_and_bottom", glazing_system_u, update_results);
+    test_optical_results("quad_layer_interior_xml_with_thermal_openings_10mm_all_sides", glazing_system_u, update_results);
 }
