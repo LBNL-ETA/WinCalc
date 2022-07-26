4e0d785a8695c8f951f9d5e108ff6ecd0766f9b0
StephenCzarnecki <sczarnecki@lbl.gov>
2022-07-25 22:57:05 -0400

    Added test for single layer perforated screen.

---
+++ test/perforated_screen.unit.cpp
@@ -0,0 +1,61 @@
+#include <memory>
+#include <gtest/gtest.h>
+#include <cstdlib>
+#include <memory>
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+
+#include "wincalc/wincalc.h"
+#include "optical_calcs.h"
+#include "util.h"
+#include "convert_optics_parser.h"
+#include "paths.h"
+
+
+using namespace wincalc;
+using namespace window_standards;
+
+class TestPerforatedScreen : public testing::Test
+{
+protected:
+    std::shared_ptr<Glazing_System> glazing_system_u;
+    std::shared_ptr<Glazing_System> glazing_system_shgc;
+
+    virtual void SetUp()
+    {
+        std::filesystem::path product_path(test_dir);
+        product_path /= "products";
+        product_path /= "perforated_screen.json";
+
+        std::vector<std::shared_ptr<OpticsParser::ProductData>> products;
+        auto product = OpticsParser::parseJSONFile(product_path.string());
+        products.push_back(product);
+
+        std::vector<Engine_Gap_Info> gaps;
+
+        std::filesystem::path standard_path(test_dir);
+        standard_path /= "standards";
+        standard_path /= "W5_NFRC_2003.std";
+        Optical_Standard standard = load_optical_standard(standard_path.string());
+		auto bsdf_hemisphere =
+			SingleLayerOptics::CBSDFHemisphere::create(SingleLayerOptics::BSDFBasis::Full);
+		 
+        glazing_system_u = std::make_shared<Glazing_System>(
+          standard, products, gaps, 1.0, 1.0, 90, nfrc_u_environments(), bsdf_hemisphere);
+        glazing_system_shgc = std::make_shared<Glazing_System>(
+          standard, products, gaps, 1.0, 1.0, 90, nfrc_shgc_environments(), bsdf_hemisphere);
+    }
+};
+
+TEST_F(TestPerforatedScreen, Test_Thermal)
+{
+    test_thermal_results("perforated_screen", "thermal_U_Environment", glazing_system_u, update_results);
+    test_thermal_results(
+      "perforated_screen", "thermal_SHGC_Environment", glazing_system_shgc, update_results);
+}
+
+TEST_F(TestPerforatedScreen, Test_Optical)
+{
+    test_optical_results("perforated_screen", glazing_system_u, update_results);
+}
