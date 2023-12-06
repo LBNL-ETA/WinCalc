d1c2eea46acf9b4ae57bc08a1952486da298f591
Simon Vidanovic <dvvidanovic@lbl.gov>
2023-12-06 14:24:40 -0800

    All pillars (including measured) are now included.

--- test/nfrc_102_nfrc_102_vacuum.unit.cpp
+++ test/nfrc_102_nfrc_102_vacuum.unit.cpp
@@ -19,7 +19,7 @@ using namespace window_standards;
 class TestNFRC102NFRC102Vacuum : public testing::Test
 {
 protected:
-    //std::shared_ptr<CIGUGapLayer> pillar_gap;
+    // std::shared_ptr<CIGUGapLayer> pillar_gap;
     std::shared_ptr<Glazing_System> glazing_system_u;
     std::shared_ptr<Glazing_System> glazing_system_shgc;
 
@@ -96,6 +96,226 @@ TEST_F(TestNFRC102NFRC102Vacuum, Test_Thermal_Spherical)
                          glazing_system_u,
                          update_results);
     test_thermal_results("NFRC_102_NFRC_102_vacuum/spherical",
+                         "thermal_SHGC_Environment",
+                         glazing_system_shgc,
+                         update_results);
+}
+
+TEST_F(TestNFRC102NFRC102Vacuum, Test_Thermal_Rectangular)
+{
+    const auto pillarLength = 0.1e-3;
+    const auto pillarWidth = 0.1e-3;
+    const auto pillarHeight = 0.1e-3;
+    const auto gapPressure = 0.1333;
+    const auto pillarConductivity = 999.0;
+    const auto pillarArea = 0.03 * 0.03;
+
+    Tarcog::ISO15099::RectangularPillar pillar{
+      pillarHeight, pillarConductivity, pillarArea, pillarLength, pillarWidth};
+
+    auto pillarGap = Tarcog::ISO15099::Layers::createPillar(pillar, gapPressure);
+
+    glazing_system_u->gap_layers({pillarGap});
+    glazing_system_shgc->gap_layers({pillarGap});
+
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/rectangular",
+                         "thermal_U_Environment",
+                         glazing_system_u,
+                         update_results);
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/rectangular",
+                         "thermal_SHGC_Environment",
+                         glazing_system_shgc,
+                         update_results);
+}
+
+
+TEST_F(TestNFRC102NFRC102Vacuum, Test_Thermal_Triangular)
+{
+    const auto pillarLength = 0.1e-3;
+    const auto pillarHeight = 0.1e-3;
+    const auto gapPressure = 0.1333;
+    const auto pillarConductivity = 999.0;
+    const auto pillarArea = 0.03 * 0.03;
+
+    Tarcog::ISO15099::PolygonalPillar pillar{pillarHeight,
+                                             pillarConductivity,
+                                             pillarArea,
+                                             pillarLength,
+                                             Tarcog::ISO15099::PolygonType::Triangle};
+
+    auto pillarGap = Tarcog::ISO15099::Layers::createPillar(pillar, gapPressure);
+
+    glazing_system_u->gap_layers({pillarGap});
+    glazing_system_shgc->gap_layers({pillarGap});
+
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/triangular",
+                         "thermal_U_Environment",
+                         glazing_system_u,
+                         update_results);
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/triangular",
+                         "thermal_SHGC_Environment",
+                         glazing_system_shgc,
+                         update_results);
+}
+
+
+TEST_F(TestNFRC102NFRC102Vacuum, Test_Thermal_Pentagon)
+{
+    const auto pillarLength = 0.1e-3;
+    const auto pillarHeight = 0.1e-3;
+    const auto gapPressure = 0.1333;
+    const auto pillarConductivity = 999.0;
+    const auto pillarArea = 0.03 * 0.03;
+
+    Tarcog::ISO15099::PolygonalPillar pillar{pillarHeight,
+                                             pillarConductivity,
+                                             pillarArea,
+                                             pillarLength,
+                                             Tarcog::ISO15099::PolygonType::Pentagon};
+
+    auto pillarGap = Tarcog::ISO15099::Layers::createPillar(pillar, gapPressure);
+
+    glazing_system_u->gap_layers({pillarGap});
+    glazing_system_shgc->gap_layers({pillarGap});
+
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/pentagon",
+                         "thermal_U_Environment",
+                         glazing_system_u,
+                         update_results);
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/pentagon",
+                         "thermal_SHGC_Environment",
+                         glazing_system_shgc,
+                         update_results);
+}
+
+TEST_F(TestNFRC102NFRC102Vacuum, Test_Thermal_Hexagon)
+{
+    const auto pillarLength = 0.1e-3;
+    const auto pillarHeight = 0.1e-3;
+    const auto gapPressure = 0.1333;
+    const auto pillarConductivity = 999.0;
+    const auto pillarArea = 0.03 * 0.03;
+
+    Tarcog::ISO15099::PolygonalPillar pillar{pillarHeight,
+                                             pillarConductivity,
+                                             pillarArea,
+                                             pillarLength,
+                                             Tarcog::ISO15099::PolygonType::Hexagon};
+
+    auto pillarGap = Tarcog::ISO15099::Layers::createPillar(pillar, gapPressure);
+
+    glazing_system_u->gap_layers({pillarGap});
+    glazing_system_shgc->gap_layers({pillarGap});
+
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/hexagon",
+                         "thermal_U_Environment",
+                         glazing_system_u,
+                         update_results);
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/hexagon",
+                         "thermal_SHGC_Environment",
+                         glazing_system_shgc,
+                         update_results);
+}
+
+TEST_F(TestNFRC102NFRC102Vacuum, Test_Thermal_LinearBearing)
+{
+    const auto pillarLength = 0.1e-3;
+    const auto pillarWidth = 0.1e-3;
+    const auto pillarHeight = 0.1e-3;
+    const auto gapPressure = 0.1333;
+    const auto pillarConductivity = 999.0;
+    const auto pillarArea = 0.03 * 0.03;
+
+    Tarcog::ISO15099::LinearBearingPillar pillar{
+      pillarHeight, pillarConductivity, pillarArea, pillarLength, pillarWidth};
+
+    auto pillarGap = Tarcog::ISO15099::Layers::createPillar(pillar, gapPressure);
+
+    glazing_system_u->gap_layers({pillarGap});
+    glazing_system_shgc->gap_layers({pillarGap});
+
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/linear_bearing",
+                         "thermal_U_Environment",
+                         glazing_system_u,
+                         update_results);
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/linear_bearing",
+                         "thermal_SHGC_Environment",
+                         glazing_system_shgc,
+                         update_results);
+}
+
+
+TEST_F(TestNFRC102NFRC102Vacuum, Test_Thermal_TruncatedCone)
+{
+    const auto pillarRadius1 = 0.1e-3;
+    const auto pillarRadius2 = 0.2e-3;
+    const auto pillarHeight = 0.1e-3;
+    const auto gapPressure = 0.1333;
+    const auto pillarConductivity = 999.0;
+    const auto pillarArea = 0.03 * 0.03;
+
+    const Tarcog::ISO15099::TruncatedConePillar pillar{
+      pillarHeight, pillarConductivity, pillarArea, pillarRadius1, pillarRadius2};
+
+    auto pillarGap = Tarcog::ISO15099::Layers::createPillar(pillar, gapPressure);
+
+    glazing_system_u->gap_layers({pillarGap});
+    glazing_system_shgc->gap_layers({pillarGap});
+
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/truncated_cone",
+                         "thermal_U_Environment",
+                         glazing_system_u,
+                         update_results);
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/truncated_cone",
+                         "thermal_SHGC_Environment",
+                         glazing_system_shgc,
+                         update_results);
+}
+
+
+TEST_F(TestNFRC102NFRC102Vacuum, Test_Thermal_AnnulusCylinder)
+{
+    const auto innerRadius = 0.1e-3;
+    const auto outerRadius = 0.2e-3;
+    const auto pillarHeight = 0.1e-3;
+    const auto gapPressure = 0.1333;
+    const auto pillarConductivity = 999.0;
+    const auto pillarArea = 0.03 * 0.03;
+
+    Tarcog::ISO15099::AnnulusCylinderPillar pillar{
+      pillarHeight, pillarConductivity, pillarArea, innerRadius, outerRadius};
+
+    auto pillarGap = Tarcog::ISO15099::Layers::createPillar(pillar, gapPressure);
+
+    glazing_system_u->gap_layers({pillarGap});
+    glazing_system_shgc->gap_layers({pillarGap});
+
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/annulus_cylinder",
+                         "thermal_U_Environment",
+                         glazing_system_u,
+                         update_results);
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/annulus_cylinder",
+                         "thermal_SHGC_Environment",
+                         glazing_system_shgc,
+                         update_results);
+}
+
+TEST_F(TestNFRC102NFRC102Vacuum, Test_Thermal_Measured)
+{
+    Tarcog::ISO15099::Glass glass1{0.005, 1.0, 0.9};
+    Tarcog::ISO15099::Glass glass2{0.005, 1.0, 0.8};
+    Tarcog::ISO15099::PillarMeasurement pillar{0.012, 0.5, 300, 350, glass1, glass2};
+
+    auto pillarGap = Tarcog::ISO15099::Layers::measuredPillar(pillar);
+
+    glazing_system_u->gap_layers({pillarGap});
+    glazing_system_shgc->gap_layers({pillarGap});
+
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/measured",
+                         "thermal_U_Environment",
+                         glazing_system_u,
+                         update_results);
+    test_thermal_results("NFRC_102_NFRC_102_vacuum/measured",
                          "thermal_SHGC_Environment",
                          glazing_system_shgc,
                          update_results);
