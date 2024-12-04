01dcb2a0ee6fca8536e30c345b4e9a71c1703c4f
Simon Vidanovic <dvvidanovic@lbl.gov>
2024-12-04 13:54:48 -0800

    Cache fix for homogenious diffuse shade

--- test/main.cpp
+++ test/main.cpp
@@ -12,7 +12,7 @@ int main( int argc, char* argv[] ) {
 #endif
 	::testing::InitGoogleTest( &argc, argv );
 	test_dir = TEST_DATA_DIR;
-	update_results = false;
+	update_results = true;
 	return RUN_ALL_TESTS();
 }
 
