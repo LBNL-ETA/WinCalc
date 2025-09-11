9d8311601309d3a908dfc2b86bfc2dceade110d5
Dragan Vidanovic <DVVidanovic@lbl.gov>
2025-09-11 09:48:23 -0700

    Fix for direct diffuse test examples.

--- test/1_layer_cgdb_31018.unit.cpp
+++ test/1_layer_cgdb_31018.unit.cpp
@@ -40,10 +40,13 @@ protected:
         standard_path /= "W5_NFRC_2003.std";
         Optical_Standard standard = load_optical_standard(standard_path.string());
 
+        auto bsdf_hemisphere =
+          SingleLayerOptics::BSDFHemisphere::create(SingleLayerOptics::BSDFBasis::Quarter);
+
         glazing_system_u = std::make_shared<Glazing_System>(
-          standard, products, gaps, 1.0, 1.0, 90, nfrc_u_environments());
+          standard, products, gaps, 1.0, 1.0, 90, nfrc_u_environments(), bsdf_hemisphere);
         glazing_system_shgc = std::make_shared<Glazing_System>(
-          standard, products, gaps, 1.0, 1.0, 90, nfrc_shgc_environments());
+          standard, products, gaps, 1.0, 1.0, 90, nfrc_shgc_environments(), bsdf_hemisphere);
     }
 };
 
