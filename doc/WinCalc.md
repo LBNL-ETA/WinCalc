6ffe991e575c35695d1f212be6268494c78fb293
Dragan Vidanovic <DVVidanovic@lbl.gov>
2026-03-30 13:07:55 -0700

    Update WindowsCalcEngine to Version_1.0.70 and update vacuum expected results

--- doc/WinCalc.md
+++ doc/WinCalc.md
@@ -6,7 +6,7 @@ graph TD
     %% Level 1 - direct dependencies
     OpticalMeasurementParser[OpticalMeasurementParser<br/><font color='red'>v2.3.7</font>]
     THMXParser[THMXParser<br/><font color='red'>v1.1.4</font>]
-    WindowsCalcEngine[WindowsCalcEngine<br/><font color='red'>Version_1.0.69</font>]
+    WindowsCalcEngine[WindowsCalcEngine<br/><font color='red'>Version_1.0.70</font>]
     Windows-CalcStandards[Windows-CalcStandards<br/><font color='red'>v1.2.2</font>]
     LibraryFEMTHERM[LibraryFEMTHERM<br/><font color='red'>Version_0.16</font>]
 
--- test/main.cpp
+++ test/main.cpp
@@ -5,14 +5,15 @@
 std::string test_dir;
 bool update_results;
 
-int main( int argc, char* argv[] ) {
+int main( int argc, char* argv[] )
+{
 #ifdef ENABLE_GTEST_DEBUG_MODE
-  ::testing::GTEST_FLAG(break_on_failure) = true;
-  ::testing::GTEST_FLAG(catch_exceptions) = false;
+    ::testing::GTEST_FLAG(break_on_failure) = true;
+    ::testing::GTEST_FLAG(catch_exceptions) = false;
 #endif
-	::testing::InitGoogleTest( &argc, argv );
-	test_dir = TEST_DATA_DIR;
-	update_results = false;
-	return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest( &argc, argv );
+    test_dir = TEST_DATA_DIR;
+    update_results = false;
+    return RUN_ALL_TESTS();
 }
 
